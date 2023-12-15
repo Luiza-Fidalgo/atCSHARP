ba7ecf917cc43bea9662d8745448ec9019f92408
Maria Luiza Fidalgo <142552463+Luiza-Fidalgo@users.noreply.github.com>
2023-12-14 21:47:20 -0300

    HomeController.cs

--- LuizaWebApp/Controller/HomeController.cs
+++ LuizaWebApp/Controller/HomeController.cs
@@ -1,6 +1,32 @@
-﻿namespace LuizaWebApp.Controller
+using LuizaWebApp.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LuizaWebApp.Controller1
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-    }
+            private readonly ILogger<HomeController> _logger;
+
+            public HomeController(ILogger<HomeController> logger)
+            {
+                _logger = logger;
+            }
+
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+            public IActionResult Privacy()
+            {
+                return View();
+            }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error()
+            {
+                return View(new ViewModelErro { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+     }
 }
