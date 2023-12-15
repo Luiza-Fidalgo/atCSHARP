df13c785a2a890a28bf2bc650bb3ccf0bdceb2e9
Maria Luiza Fidalgo <142552463+Luiza-Fidalgo@users.noreply.github.com>
2023-12-14 21:49:32 -0300

    Flor.cs

--- LuizaWebApp/Model/Flor.cs
+++ LuizaWebApp/Model/Flor.cs
@@ -2,7 +2,7 @@
 {
     public class Flor
     {
-          public string Nome { get; set; }
+        public string Nome { get; set; }
         public string Especie { get; set; }
         public double Preço { get; set; }
         public int QuantidadeEstoque { get; set; }
